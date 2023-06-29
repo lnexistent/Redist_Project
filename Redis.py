daae1da243c295889435ea9c45a32ddc7a4f9a12
Roberto Biggi <r.biggi@itsrizzoli.it>
2023-06-29 11:40:42 +0200

    function Submit_idea
    submission of an idea and check of similar ideas, check if idea is identical

--- Redis.py
+++ Redis.py
@@ -43,3 +43,61 @@ def get_ideas(redis_conn):
             idea_dict[idea_name].append(name.decode())
 
     return idea_dict
+
+def submit_idea(redis_conn):
+    while True:
+        idea = input('Enter your idea (type "/quit" to exit): ')
+        if idea.strip() == '/quit':
+            return False
+        elif idea.strip() == '':
+            print('Invalid input. Please enter your idea.')
+        else:
+            break
+
+    # Check for similar ideas
+    ideas = get_ideas(redis_conn)
+    similar_ideas = []
+    for existing_idea in ideas:
+        similarity = SequenceMatcher(None, idea.lower(), existing_idea.lower()).ratio()
+        if similarity >= 0.6:
+            similar_ideas.append(existing_idea)
+
+    if similar_ideas:
+        print('Warning: The submitted idea is similar to the following existing ideas (use /quit to exit):')
+        for i, similar_idea in enumerate(similar_ideas, start=1):
+            print(f'{i}. {similar_idea}')
+
+        while True:
+            choice = input('Do you want to continue with this idea? (y/n): ')
+            if choice.lower() == 'n':
+                return submit_idea(redis_conn)  # Ask the user to enter a new idea
+            elif choice.lower() == 'y':
+                break
+            else:
+                print('Invalid choice. Please enter either "y" or "n".')
+
+    while True:
+        names = input('Enter the name(s) of the person(s) who made the idea (comma-separated) (use /quit to exit): ')
+        if names.strip() == '':
+            print('Invalid input. Please enter the name(s) of the person(s).')
+        else:
+            break
+
+    names_list = [name.strip() for name in names.split(',')]
+    names_string = ', '.join(names_list)
+
+    if idea in ideas:
+        # If the idea already exists, append the names to the existing idea
+        existing_names = redis_conn.smembers(f'idea_names:{idea}')
+        for name in names_list:
+            redis_conn.sadd(f'idea_names:{idea}', name)
+    else:
+        # If the idea is new, add it to the database with 0 votes
+        redis_conn.hset('ideas', idea, 0)
+        # Store the names of people who made the idea in a set
+        for name in names_list:
+            redis_conn.sadd(f'idea_names:{idea}', name)
+
+    redis_conn.lpush(names_string, f'Idea submitted: {idea}')
+    print('Idea submitted successfully!')
+    return True
